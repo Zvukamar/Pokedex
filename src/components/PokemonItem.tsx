8c315c582040cdec3f629af2b83b23490f1d6fe2
Alex Havkin <alex.h@onoapps.com>
2023-04-17 12:53:29 +0300

    list alignment

--- src/components/PokemonItem.tsx
+++ src/components/PokemonItem.tsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        marginBottom: 24,
+        marginVertical: 12,
         gap: 12
     },
     text: {
