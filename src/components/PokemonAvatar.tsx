ced94905cdf0bedf33d93b9c23fb09961f1649d3
Alex Havkin <alex.h@onoapps.com>
2023-04-17 18:29:33 +0300

    set default image in case image was not found 404

--- src/components/PokemonAvatar.tsx
+++ src/components/PokemonAvatar.tsx
@@ -1,20 +1,26 @@
 import { Image, ImageStyle, StyleProp, StyleSheet } from 'react-native';
 import useTheme from '../hooks/useTheme';
+import { useState } from 'react';
 
 interface PokemonAvatarProps {
     uri: string;
     style?: StyleProp<ImageStyle>;
 }
 
+const defaultImageUrl = 'https://play-lh.googleusercontent.com/MDaSgXlbRkftfjNIdJ2oHodVBVLOmVg2PevfdzJkbtlawfMA-8gMAs-kCfXXY5XyLw';
+
 const PokemonAvatar = ({ uri, style }: PokemonAvatarProps) => {
     const theme = useTheme();
     const styles = createStyle(theme);
+
+    const [imageUri, setImageUri] = useState(uri)
 
     return (
         <Image
             style={[styles.itemImage, style]}
             resizeMode='contain'
-            source={{ uri }}
+            onError={() => setImageUri(defaultImageUrl)}
+            source={{ uri: imageUri }}
         />
     )
 }
