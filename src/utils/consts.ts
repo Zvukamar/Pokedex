deed80dbc69b60f3a3e9b28ae665356569ab8a79
Alex Havkin <alex.h@onoapps.com>
2023-04-18 10:32:22 +0300

    fixed localhosst for android

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,1 +1,4 @@
-export const baseUrl = 'http://localhost:8080';
+import { Platform } from 'react-native';
+
+const isIos = Platform.OS === 'ios';
+export const baseUrl = isIos ? 'http://localhost:8000' : 'http://10.0.2.2:8080';
